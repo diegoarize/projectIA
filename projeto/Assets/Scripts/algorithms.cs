6a3ee8c85192bbbb841800e932031b782d1a16c6
diegoarize <diegoarize@outlook.com>
2015-04-02 19:03:02 -0300

    DFS implementado em Algorithms
    tentando fazer commit somente de arquivos que modificamos, ignora o
    commit de arquivos de projeto

---
+++ projeto/Assets/Scripts/algorithms.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class algorithms : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public List<Transform> DFS(Transform firstNode, Transform lastNode)
+    {
+		List<Transform> l = null;
+		if (firstNode == lastNode) {
+			l = new List<Transform> ();
+			l.Add (firstNode);
+			return l;
+		} else {
+			foreach (Transform n in firstNode.GetComponent<Node>().successors) {
+				l = l.Concat(DFS(n, lastNode)).ToList();
+			}
+		}
+		return l;
+    }
+}
